959dcc58d690db14d957cb902c3442ac583b5d85
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-12-17 00:36:34 -0600

    Added file menu
    I am still new to this environment.  It seems adjacent to XCode, so I can figure my way around and get somewhere.  However, trying to find the right design is still travial at this present time.
    
    This commit mainly has the file menu and using proper constraints - I hope.  Most of the options are dead-ends as I have yet to implement them fully.  I will need to figure out how to make forms connect with other forms, like ViewControllers linking to other ViewControllers via Segues (XCode).

---
+++ ZDoomStarter/ZDoomStarter/MainWindow.xaml.vb
@@ -0,0 +1,40 @@
+﻿Class MainWindow
+    ' Main File Menu: File Menu
+    Private Sub FileMenu_Click(sender As Object, e As RoutedEventArgs)
+        ' Do nothing?
+        ' Removing this causes compilation issues, I'll need to figure out why later.
+    End Sub
+
+
+
+    ' Destroy
+    Private Sub Menu_Exit_Click(sender As Object, e As RoutedEventArgs) Handles FileMenuExit.Click
+        ' When clicked, just immediately close the program.
+        Close()
+    End Sub
+
+    ' Configure Source Ports
+    Private Sub ConfigureMenuSourcePorts_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuSourcePorts.Click
+        ' TODO: Implement this
+    End Sub
+
+    ' Configure IWAD's
+    Private Sub ConfigureMenuIWADs_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuIWADs.Click
+        ' TODO: Implement this
+    End Sub
+
+    ' Configure PWAD Directory Path
+    Private Sub ConfigureMenuPWADsDir_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuPWADsDir.Click
+        ' TODO: Implement this
+    End Sub
+
+    ' Allow the end-user to check for updates
+    Private Sub HelpMenuUpdatesGitHub_Click(sender As Object, e As RoutedEventArgs)
+        ' TODO: Implement this
+    End Sub
+
+    ' Display software information
+    Private Sub HelpMenuAbout_Click(sender As Object, e As RoutedEventArgs)
+        ' TODO: Implement this
+    End Sub
+End Class
