17954e89bf61888d8fe8591af109bdc357c9ac17
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-12-19 22:26:17 -0600

    Removed 'FileMenu_Click' Action
    Expunged the action and function call 'FileMenu_Click'.  This action was not necessary, I am still learning how this works.

--- ZDoomStarter/ZDoomStarter/MainWindow.xaml.vb
+++ ZDoomStarter/ZDoomStarter/MainWindow.xaml.vb
@@ -1,12 +1,4 @@
 ﻿Class MainWindow
-    ' Main File Menu: File Menu
-    Private Sub FileMenu_Click(sender As Object, e As RoutedEventArgs)
-        ' Do nothing?
-        ' Removing this causes compilation issues, I'll need to figure out why later.
-    End Sub
-
-
-
     ' Destroy
     Private Sub Menu_Exit_Click(sender As Object, e As RoutedEventArgs) Handles FileMenuExit.Click
         ' When clicked, just immediately close the program.
