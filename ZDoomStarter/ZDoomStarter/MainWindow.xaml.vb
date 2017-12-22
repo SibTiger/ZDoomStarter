bc36f88c70616e721ee62deea4a7fd336489e4f9
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-12-21 20:33:43 -0600

    Revised documentation; preparation
    Preparing for a move-in, once the foundation has been set.

--- ZDoomStarter/ZDoomStarter/MainWindow.xaml.vb
+++ ZDoomStarter/ZDoomStarter/MainWindow.xaml.vb
@@ -1,6 +1,19 @@
 ﻿Class MainWindow
+    ' Declarations and Initializations
+    ' -------------------------------------------------
+    ' -------------------------------------------------
+
+
+
+    ' UI ELEMENTS
+    ' =================================================
+    ' =================================================
+    ' =================================================
+
+
+
     ' File Menu: Destroy
-    ' ==========================================
+    ' ------------------------------------------
     ' This will close the program properly.
     Private Sub Menu_Exit_Click(sender As Object, e As RoutedEventArgs) Handles FileMenuExit.Click
         ' When clicked, just immediately close the program.
@@ -8,8 +21,10 @@
     End Sub
 
 
+
+
     ' Configure File Menu: Configure Source Ports
-    ' ==========================================
+    ' ------------------------------------------
     Private Sub ConfigureMenuSourcePorts_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuSourcePorts.Click
 
         ' Create the form instance
@@ -30,29 +45,37 @@
     End Sub
 
 
+
+
     ' Configure File Menu: Configure IWAD's
-    ' ==========================================
+    ' ------------------------------------------
     Private Sub ConfigureMenuIWADs_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuIWADs.Click
         ' TODO: Implement this
     End Sub
 
 
+
+
     ' Configure File Menu: Configure PWAD Directory Path
-    ' ==========================================
+    ' ------------------------------------------
     Private Sub ConfigureMenuPWADsDir_Click(sender As Object, e As RoutedEventArgs) Handles ConfigureMenuPWADsDir.Click
         ' TODO: Implement this
     End Sub
 
 
+
+
     ' Help File Menu: Allow the end-user to check for updates
-    ' ==========================================
+    ' ------------------------------------------
     Private Sub HelpMenuUpdatesGitHub_Click(sender As Object, e As RoutedEventArgs)
         ' TODO: Implement this
     End Sub
 
 
+
+
     ' Help File Menu: Display software information
-    ' ==========================================
+    ' ------------------------------------------
     Private Sub HelpMenuAbout_Click(sender As Object, e As RoutedEventArgs)
         ' TODO: Implement this
     End Sub
