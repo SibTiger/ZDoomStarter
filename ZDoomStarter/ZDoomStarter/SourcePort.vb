81fbeae420a32117e44738b14f14f6885422e3dc
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-12-21 20:25:23 -0600

    Added 'SourcePort' Class Obj.
    Added a new class 'SourcePort'.  This class will be necessary for holding information regarding the Source Ports that the user declares available to this program.

---
+++ ZDoomStarter/ZDoomStarter/SourcePort.vb
@@ -0,0 +1,34 @@
+﻿Public Class SourcePort
+    ' Declarations and Initializations
+    ' -------------------------------------------------
+    Dim absolutePath As String      ' Absolute path to that executable
+    Dim niceName As String          ' Name of that executable
+    Dim CustomNotes As String       ' Optional notes provided by the end-user
+    ' -------------------------------------------------
+
+
+
+
+    ' Default Constructor
+    ' -------------------------------------------------
+    ' The default constructor for this class; implicit values will be given once instantiated.
+    Public Sub New()
+        absolutePath = Nothing      ' Null Value
+        niceName = Nothing          ' Null Value
+        CustomNotes = Nothing       ' Null Value
+    End Sub
+
+
+
+
+    ' Constructor
+    ' -------------------------------------------------
+    ' Explicit constructor for this class; this will set the values accordingly when instantiated properly.
+    Public Sub New(path As String,
+                   name As String,
+                   note As String)
+        absolutePath = path
+        niceName = name
+        CustomNotes = note
+    End Sub
+End Class
