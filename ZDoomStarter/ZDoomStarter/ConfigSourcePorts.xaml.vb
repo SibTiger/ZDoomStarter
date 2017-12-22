27308ffd19e1d2495cd79500864cc06a84a2446d
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-12-21 20:35:17 -0600

    Revised documentation and UI Names
    Changed the UI action names to follow standard convention naming scheme.
    Revised documentation

--- ZDoomStarter/ZDoomStarter/ConfigSourcePorts.xaml.vb
+++ ZDoomStarter/ZDoomStarter/ConfigSourcePorts.xaml.vb
@@ -1,3 +1,62 @@
 ﻿Public Class ConfigSourcePorts
+    ' Declarations and Initializations
+    ' -------------------------------------------------
+    ' -------------------------------------------------
 
+
+
+    ' UI ELEMENTS
+    ' =================================================
+    ' =================================================
+    ' =================================================
+
+
+
+
+    ' Cancel [BUTTON]
+    ' ------------------------------------------
+    ' When clicked, this will merely close the configuration window and cancel all pending alterations.
+    Private Sub ButtonCancel_Click(sender As Object, e As RoutedEventArgs) Handles ButtonCancel.Click
+        Close()
+    End Sub
+
+
+
+
+    ' Okay [BUTTON]
+    ' ------------------------------------------
+    ' When clicked, this will save all pending changes and close the configuration window.
+    Private Sub ButtonOK_Click(sender As Object, e As RoutedEventArgs) Handles ButtonOK.Click
+
+    End Sub
+
+
+
+
+    ' Expunge Entry [BUTTON]
+    ' ------------------------------------------
+    ' When clicked and an entry has been selected within the list, the entry (or row) will be thrashed off the array.
+    Private Sub ButtonDelete_Click(sender As Object, e As RoutedEventArgs) Handles ButtonDelete.Click
+
+    End Sub
+
+
+
+
+    ' Add Entry [BUTTON]
+    ' ------------------------------------------
+    ' When clicked, this will allow the end-user to select an executable engine and add that to the list.
+    Private Sub ButtonAdd_Click(sender As Object, e As RoutedEventArgs) Handles ButtonAdd.Click
+
+    End Sub
+
+
+
+
+    ' Source Ports Available [List Box]
+    ' ------------------------------------------
+    ' Displays all source ports available to this program; provided by the end-user (or through a configuration file or entity).
+    Private Sub ListView_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles ListView.SelectionChanged
+
+    End Sub
 End Class
