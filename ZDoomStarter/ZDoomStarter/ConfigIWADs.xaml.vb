a4262ab4735a777bbd89237e9cf3e3bdcf1c77be
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-01-01 04:45:40 -0600

    Added class description
    Applied a class comment description as to what the purpose is for this particular class.

--- ZDoomStarter/ZDoomStarter/ConfigIWADs.xaml.vb
+++ ZDoomStarter/ZDoomStarter/ConfigIWADs.xaml.vb
@@ -1,3 +1,14 @@
-﻿Public Class ConfigIWADs
+﻿' Configure IWADs [UI]
+' ---------------------------------------
+' =======================================
+' This class is designed to allow the end-user to customize the IWAD
+' list that this program is allowed to invoke.  With that, the user
+' can add or remove the IWAD's within the IWAD list.
+' ---------------------------------------
+
+
+
+
+Public Class ConfigIWADs
 
 End Class
