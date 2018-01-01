d9547849d048a7ba9bdeb3a71b02dd9e584bc92f
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-01-01 17:24:04 -0600

    WIP: Progress Update - Populating functionality
    Duplicating module functions from Source Port class to this particular class.
    
    Very WIP

--- ZDoomStarter/ZDoomStarter/ConfigIWADs.xaml.vb
+++ ZDoomStarter/ZDoomStarter/ConfigIWADs.xaml.vb
@@ -10,5 +10,241 @@
 
 
 Public Class ConfigIWADs
+#Region "Declarations and Initializations"
+    ' Default cached index value
+    ' The default value will signify that nothing was selected in the ViewList.
+    Private cachedIndexDefault As Int32 = -1
 
+    ' Hold the index highlighted by the end-user from the ViewList UI component.
+    Private viewListSelectedIndex As Int32 = cachedIndexDefault
+
+    ' When available, this will hold the file's absolute path that was selected by the end-user.
+    ' This is only modified when the user selects a file.
+    Private fileAbsolutePath As String = Nothing
+
+    ' When available, this will hold the file's name that was selected by the end-user.
+    ' This is only modified when the user selects a file.
+    Private fileSafeName As String = Nothing
+
+    ' This list will hold all IWAD entries that is available to this program.
+    ' NOTE: Property (or {get; set;}) is adjacent to a function call, hence the different naming scheme.
+    Public Property DisplayIWADList As New List(Of IWAD)
+
+    ' Exit flag; when the user clicks on either cancel or okay button, this variable will dictate rather
+    ' the IWAD list will be updated or remain as-is.
+    ' Meaning of the values:
+    '   Cancel = False
+    '       Nothing is updated
+    '   OK = True
+    '       Update the source port list
+    Public updateIWADList As New Boolean
+#End Region
+
+
+
+
+    ' Constructor Window
+    ' ------------------------------------------
+    ' This is the default constructor for this window.
+    ' This is ideal for transferring data from one end to another.
+    ' -----------------------
+    ' Parameters:
+    '   iwadList [List<T>(IWAD)]
+    '       HOlds all IWAD's available to this program as declared by the end-user.
+    Public Sub New(ByVal iwadList As List(Of IWAD))
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Use the incoming IWAD list for possible modification if needed.
+        DisplayIWADList = New List(Of IWAD)(iwadList)
+    End Sub
+
+
+
+
+
+    ' Browse UI Executable
+    ' ------------------------------------------
+    ' This function is dedicated for managing the Browsing Dialog Window.  When the user selects an '.wad' (or other supported archives data files) file, all information necessary is recorded in the appropriate variables:
+    ' fileAbsolutePath - Used for capturing the entire absolute path of the selected file.
+    ' fileSafeName - Used for capturing the name of the selected file with extension applied.
+    ' ----
+    ' SUPPORTED EXTENSIONS:
+    '  Please see this: https://zdoom.org/wiki/Archive_format
+    '  Remarks: "[...] any archive format supported by ZDoom can technically be an IWAD." - https://zdoom.org/wiki/IWAD
+    '   > .WAD
+    '   > .GRP
+    '   > .RFF
+    '   > .PAK
+    '   > .ZIP
+    '   > .PK3 [implies: .ZIP]
+    '   > .IPK3 [implies: .ZIP]
+    '   > .7z
+    '   > .PK7 [implies: .7z]
+    '   > .IPK7 [implies: .7z]
+    '
+    ' UNSUPPORTED:
+    '   > Directories
+    '       I don't have much time on this project, I will not be able to quickly get this one finished at time.
+    '   > Any File
+    '       For sheer User-Friendliness and simplicity, I can not support this.  Not only is this vague to all files but and more importantly - this can confuse some end-users.
+    '   
+    ' -----------------------
+    ' Output:
+    '   Boolean
+    '       True: User canceled or an error occurred during the process.
+    '       False: User selected a file and all information is recorded and ready.
+    Private Function BrowseUIExecutable() As Boolean
+        ' Declarations and Initializations
+        ' ----------------------------------
+        Dim browseFileDialog As New _
+            Microsoft.Win32.OpenFileDialog()    ' Create an instance of the OpenFileDialog
+
+        Dim filterSearch = "WAD|*.wad, GRP|*.grp, RFF|*.rff, PAK|*.pak, ZIP|*.zip, PK3|*.pk3, IPK3|*.ipk3, 7z|*.7z, PK7|*.pk7, IPK7|*.ipk7"   ' Used for limiting our search to only the selected extensions
+        ' ----------------------------------
+
+        ' Setup the properties for the OpenFileDialog() instance
+        browseFileDialog.Filter = filterSearch              ' Filter the search.
+
+        ' Open the OpenFileDialog() browse window
+        If (browseFileDialog.ShowDialog()) Then
+            ' Successful result
+            fileAbsolutePath = browseFileDialog.FileName    ' Selected file absolute path
+            fileSafeName = browseFileDialog.SafeFileName    ' Selected file name
+            Return False
+        Else
+            ' User Canceled or denied
+            ' Reset to nothing for assurance
+            fileAbsolutePath = Nothing
+            fileSafeName = Nothing
+            Return True
+        End If
+    End Function
+
+
+
+
+    ' Filter Name - Extension
+    ' ------------------------------------------
+    ' This function is dedicated to filtering the incoming file name by removing the file extension.
+    ' -----------------------
+    ' Parameters:
+    '   name [String]
+    '       Incoming file name to be filtered.
+    Private Function FilterNameExtension(fileName As String) As String
+        Return System.IO.Path.GetFileNameWithoutExtension(fileName)
+    End Function
+
+
+
+
+    ' Get User Notes
+    ' ------------------------------------------
+    ' This function is designed to get user notes regarding the selected file.
+    ' If the user provides no feedback whatsoever, then a default value will take place to assure some sort of data is being sent back.
+    ' -----------------------
+    ' Output:
+    '   String
+    '       Customized notes provided by the end-user.
+    Private Function GetUserNotes() As String
+        ' Declarations and Initializations
+        ' ----------------------------------
+        Dim defaultReturnValue As String = "<<EMPTY>>"
+        Dim feedback As String
+        ' ----------------------------------
+
+        ' Yes, we are going to use the InputBox().  I know its old,
+        ' but it does exactly what we are looking for here.
+        feedback = InputBox(
+            "(OPTIONAL) Add notes for this IWAD.  For example: Doom 2: Hell on Earth, Ultimate Doom, or a custom IWAD name",
+            "Add Customized Notes")
+
+        ' If in case the user does NOT provide any feedback, then insert a default value.
+        If (String.IsNullOrEmpty(feedback)) Then
+            feedback = defaultReturnValue
+        End If
+
+        ' Return the notes back to the calling function
+        Return feedback
+    End Function
+
+
+
+
+    ' Capitalize First Char
+    ' ------------------------------------------
+    ' This function will take the requested string and capitalize the first character.
+    ' For example: "my cat loves to sleep." --> "My cat loves to sleep."
+    ' -----------------------
+    ' Parameters:
+    '   str [String]
+    '       The requested string that will have the first character capitalized.
+    ' -----------------------
+    ' Output:
+    '   String
+    '       The new string that contains the first capitalization.
+    '       NOTE: If the string is empty, null, or contains a space in the first char index, then
+    '           the string itself will be sent back with no modifications.
+    Private Function CapitalizeFirstChar(str As String) As String
+        ' Declarations and Initializations
+        ' ----------------------------------
+        Dim charArr() As Char
+        ' ----------------------------------
+
+        ' First make sure that the string is not empty nor contains
+        ' whitespace at the first index of the string (array).
+        If (String.IsNullOrWhiteSpace(str)) Then
+            Return str  ' We can't do anything with this; return as-is.
+        End If
+
+        ' Convert the string to a char-array
+        charArr = str.ToCharArray
+
+        ' Capitalize the first character
+        charArr(0) = Char.ToUpper(charArr(0))
+
+        ' Return the character array (as a string).
+        Return New String(charArr)
+    End Function
+
+
+
+
+    ' UI ELEMENTS
+    ' =================================================
+    ' =================================================
+    ' =================================================
+
+    Private Sub ButtonAdd_Click(sender As Object, e As RoutedEventArgs) Handles ButtonAdd.Click
+
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As RoutedEventArgs) Handles ButtonDelete.Click
+
+    End Sub
+    ' Okay [BUTTON]
+    ' ------------------------------------------
+    ' When clicked, this will save all pending changes and close the configuration window.
+    Private Sub ButtonOK_Click(sender As Object, e As RoutedEventArgs) Handles ButtonOK.Click
+        updateIWADList = True       ' Save the IWAD list by overwriting the existing.
+        Close()                     ' Close the child window
+    End Sub
+
+    ' Cancel [BUTTON]
+    ' ------------------------------------------
+    ' When clicked, this will merely close the configuration window and cancel all pending alterations.
+    Private Sub ButtonCancel_Click(sender As Object, e As RoutedEventArgs) Handles ButtonCancel.Click
+        updateIWADList = False      'Do not save IWAD list; keep the existing.
+        Close()                     ' Close the child window
+    End Sub
+
+
+    ' List IWAD - Item Selected [EVENT]
+    ' ------------------------------------------
+    ' This function will be automatically called when the user clicks an item from the ViewList.
+    ' When clicked, this will cache the index that was selected.  We may use that index when the user clicks on the 'Remove' UI button.
+    Private Sub ListView_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+        ' Cache the selected index
+        viewListSelectedIndex = ListIWADs.SelectedIndex
+    End Sub
 End Class
