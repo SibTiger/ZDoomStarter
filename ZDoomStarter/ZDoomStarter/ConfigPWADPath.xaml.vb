eb7a8daf2d9b3a5bf7516a7b83aa22529134d40c
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-01-02 16:42:42 -0600

    Provided class description
    Provided class description for the 'ConfigPWADPath'.  This will provide information as to what the purpose is for this particular class.

--- ZDoomStarter/ZDoomStarter/ConfigPWADPath.xaml.vb
+++ ZDoomStarter/ZDoomStarter/ConfigPWADPath.xaml.vb
@@ -1,3 +1,16 @@
-﻿Public Class ConfigPWADPath
+﻿' Configure PWAD Directory [UI]
+' ---------------------------------------
+' =======================================
+' This class is designed to allow the end-user to customize the
+' directory location of where the PWADs are located.
+' This program will not do another drastic with this information,
+' but merely designed to make life easier for the end-user when
+' he or she adds PWADs into their game setup.
+' ---------------------------------------
+
+
+
+
+Public Class ConfigPWADPath
 
 End Class
